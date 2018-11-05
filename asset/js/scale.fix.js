1da73fd49f6910d18c9a76af7afc2813c2c48fc5
sunilpaulmathew <sunil.kde@gmail.com>
2018-11-05 17:12:06 +0900

    Main: overall UI enhancements
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- asset/js/scale.fix.js
+++ asset/js/scale.fix.js
@@ -24,13 +24,4 @@
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
-}
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
 })(document);
